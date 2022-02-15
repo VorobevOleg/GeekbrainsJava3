d40d8efe61f21d0e19fb2fbc66269000346992ea
VorobevOleg <kukuruza-09@ya.ru>
2022-02-15 16:19:23 +0300

    v1

---
+++ MagicChat/server/src/main/java/server/Server.java
@@ -0,0 +1,108 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private ServerSocket server;
+    private Socket socket;
+    private final int PORT = 8189;
+
+    private List<ClientHandler> clients;
+    private DbAuthService authService;
+
+    private ExecutorService thredService = Executors.newCachedThreadPool();
+
+    public ExecutorService getThredService() {
+        return thredService;
+    }
+
+    public Server() {
+        clients = new CopyOnWriteArrayList<>();
+        authService = new DbAuthService();
+
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Server started!");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Client connected: " + socket.getRemoteSocketAddress());
+                new ClientHandler(this, socket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Server stop");
+            try {
+                thredService.shutdown();
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void subscribe(ClientHandler clientHandler) {
+        clients.add(clientHandler);
+        broadcastClientList();
+    }
+
+    public void unsubscribe(ClientHandler clientHandler) {
+        clients.remove(clientHandler);
+        broadcastClientList();
+    }
+
+    public void broadcastMsg(ClientHandler sender, String msg) {
+        String message = String.format("[ %s ]: %s", sender.getNickname(), msg);
+        for (ClientHandler c : clients) {
+            c.sendMsg(message);
+        }
+    }
+
+    public void privateMsg(ClientHandler sender, String receiver, String msg) {
+        String message = String.format("[ %s ] to [ %s ]: %s", sender.getNickname(), receiver, msg);
+        for (ClientHandler c : clients) {
+            if (c.getNickname().equals(receiver)) {
+                c.sendMsg(message);
+                if (!sender.getNickname().equals(receiver)) {
+                    sender.sendMsg(message);
+                }
+                return;
+            }
+        }
+        sender.sendMsg("not found user: " + receiver);
+    }
+
+    public boolean isLoginAuthenticated(String login) {
+        for (ClientHandler c : clients) {
+            if (c.getLogin().equals(login)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void broadcastClientList() {
+        StringBuilder sb = new StringBuilder("/clientlist");
+
+        for (ClientHandler c : clients) {
+            sb.append(" ").append(c.getNickname());
+        }
+
+        String message = sb.toString();
+
+        for (ClientHandler c : clients) {
+            c.sendMsg(message);
+        }
+    }
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+}
